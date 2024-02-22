2e1efacbb80ceda5a59153ee19f29e2098284507
sfNaomi <122491121+sfNaomi@users.noreply.github.com>
2024-02-22 13:08:48 +0000

    Added portfolio step2

---
+++ build/precache-manifest.dd84f0e2c581a677435ec489213cb6ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793d09ddeee3498277b3d047118090a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aecb1ddb46f134b15f2b",
+    "url": "/static/css/2.1cbe51a9.chunk.css"
+  },
+  {
+    "revision": "a9aa557e96bdc420ee7b",
+    "url": "/static/css/main.38a4c9c3.chunk.css"
+  },
+  {
+    "revision": "aecb1ddb46f134b15f2b",
+    "url": "/static/js/2.aac0fca3.chunk.js"
+  },
+  {
+    "revision": "a9aa557e96bdc420ee7b",
+    "url": "/static/js/main.f4b7aaa3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
